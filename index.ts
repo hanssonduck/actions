de056b3b62b20f1fa66d23edd158d26f8cdecaf3
Isak <16906089+hanssonduck@users.noreply.github.com>
2024-05-09 01:33:15 +0200

    Update index.ts (#1)

--- index.ts
+++ index.ts
@@ -2,6 +2,6 @@ import type { Serve } from "bun";
 import { Hono } from "hono";
 
 const app = new Hono();
-app.get("/", ({ json }) => json({ message: "Hello universe!" }));
+app.get("/", ({ json }) => json({ message: "Hello world!" }));
 
 export default app satisfies Serve;
