125ca0cf05392951aeb6ae56b5261a83d4846236
Isak <16906089+hanssonduck@users.noreply.github.com>
2024-05-08 19:57:54 +0200

    Reverse hello world message

--- index.ts
+++ index.ts
@@ -2,6 +2,6 @@ import type { Serve } from "bun";
 import { Hono } from "hono";
 
 const app = new Hono();
-app.get("/", ({ json }) => json({ message: "Hello Universe!" }));
+app.get("/", ({ json }) => json({ message: "Hello World!" }));
 
 export default app satisfies Serve;
